b247dc91655decc301ef349b1011e4e4776c8762
Jason <jsonwu@qq.com>
2018-04-04 16:44:26 +0800

    歌单图片懒加载

--- src/main.js
+++ src/main.js
@@ -3,11 +3,17 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
 
 import 'common/stylus/index.styl'
 
 // 调用fastclick，attach到body，让body下的所有div点击都没有3秒的延迟
 fastclick.attach(document.body)
+
+// 图片懒加载
+Vue.use(VueLazyLoad, {
+  loading: require('common/image/default.png')
+})
 
 /* eslint-disable no-new */
 new Vue({
