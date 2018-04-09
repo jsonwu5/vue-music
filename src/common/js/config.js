9aa54abbba3915d3ecf4d8875b9b4a1f845a09f0
Jason <jsonwu@qq.com>
2018-04-09 14:07:31 +0800

    播放器vuex数据设计

---
+++ src/common/js/config.js
@@ -0,0 +1,9 @@
+// 配置文件
+export const playMode = {
+  // 顺序播放
+  sequence: 0,
+  // 单曲循环
+  loop: 1,
+  // 随机播放
+  random: 2
+}
--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,19 @@
+import {playMode} from 'common/js/config'
+
 const state = {
-  singer: {}
+  singer: {},
+  // 播放状态
+  playing: false,
+  // 全屏状态
+  fullScreen: false,
+  // 播放列表
+  playlist: [],
+  // 顺序列表
+  sequenceList: [],
+  // 播放模式
+  mode: playMode.sequence,
+  // 当前播放歌曲
+  currentIndex: -1
 }
 
 export default state
